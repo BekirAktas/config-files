d0eafc69585065788cff44c6a164195291ee17aa
Bekir Aktas <bekir.aktas@hubx.co>
2023-07-04 16:35:51 +0300

    feat: add new keymap

--- nvim/lua/bekir/remap.lua
+++ nvim/lua/bekir/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) 
 vim.api.nvim_set_keymap('n', '<leader>b', ':NvimTreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeFocus<CR>', {noremap = true})
 
